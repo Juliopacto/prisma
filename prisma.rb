2870faf6d279678ad72c9755be8849a4e77793f5
codeamac12 <codeamac12@gmail.com>
2016-07-21 20:02:36 -0500

    prisma up 1

---
+++ prisma.rb
@@ -0,0 +1,16 @@
+def prism_type(a, b, c) #nombre del método con tres parámetros
+  if a != b && b != c && a != c #se le dice a la condición, que si todas son diferentes, manda imprimir "cuboide"
+    p "cuboide"
+  elsif a == b && b != c && c != a #si en la condición son dos lados iguales manda imprimir "prisma rectangular"
+    p "prisma rectangular"
+  else a == b && b == c && c == a #si en la condición los tres lados son iguales manda imprimir "cubo"
+    p "cubo"
+  end  
+end
+
+# Pruebas
+p prism_type(5, 5, 5) == "cubo"
+p prism_type(5, 5, 4) == "prisma rectangular"
+p prism_type(5, 4, 3) == "cuboide"
+p prism_type(10, 8, 2) == "cuboide"
+
